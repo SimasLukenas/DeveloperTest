034ce0d44ddfb5ce96992d7f41f49d8c49cff907
Simas Lukėnas <s_l456@txstate.edu>
2019-07-19 16:38:05 +0300

    Initial Commit
    Added DeveloperTest .Net Core project using Visual Studio.

---
+++ DeveloperTest/DeveloperTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace DeveloperTest
+    {
+    class Program
+        {
+        static void Main (string[] args)
+            {
+            if (args.Length != 1)
+                {
+                Console.WriteLine ("Usage: dotnet DeveloperTest.dll \"Test string to do calculations with\"");
+                return;
+                }
+
+            // Using first argument as input string.
+            StringHelper.CountWords (args[0], out var wordCountDictionary, out var wordLengthContDictionary);
+
+
+            // Print outputs
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Setting console to print unicode.
+
+            Console.WriteLine ($"List: {string.Join(", ", wordCountDictionary.Select (kvp => $"\"{kvp.Key}, {kvp.Value}\""))}");
+            Console.WriteLine ($"List: {string.Join(", ", wordLengthContDictionary.Select (kvp => $"\"{kvp.Key}, {kvp.Value}\""))}");
+            Console.WriteLine ("Press Enter to continue...");
+            Console.ReadLine ();
+            }
+        }
+    }
---
+++ DeveloperTest/DeveloperTest/StringHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeveloperTest
+    {
+    public static class StringHelper
+        {
+        public static void CountWords (string input, out Dictionary<string, int> wordCountDictionary, out SortedDictionary<int, int> wordLengthCountDictionary)
+            {
+            // Could not use whitespace as separators because text might include punctuation.
+            string[] separators = { ",", ".", "!", "?", ";", ":", "/", "\\", "\'", "\"", "(", ")", "[", "]", "<", ">", " ", "\t", "\r", "\n" };
+            var words = input.Split (separators, StringSplitOptions.RemoveEmptyEntries);
+
+            wordCountDictionary = words.GroupBy (word => word).ToDictionary (w => w.Key, w => w.Count ());
+
+            // Using SortedDictionary so the results would be sorted for readability.
+            wordLengthCountDictionary = new SortedDictionary<int, int> (words.GroupBy (word => word.Length).ToDictionary (w => w.Key, w => w.Count ()));
+            }
+
+        // Function for 2nd task
+        public static string foo (string argument)
+            {
+            // Function performs string reversal.
+            // Removed call stack limitations.
+            // Using reverse iterator to create new string.
+            return new string (argument.Reverse ().ToArray ());
+            }
+
+        }
+    }
